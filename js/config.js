e466dc5bb4b07065a898dd63fe6cfb9f15049b4d
mis-Darpan <mis@litpaxtechnology.com>
2026-06-30 10:18:35 +0530

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+// =============================================
+// LITPAX ADVANCE SYSTEM — config.js
+// Sirf yahi file edit karni hai URL change ke liye
+// =============================================
+
+const CONFIG = {
+  GAS_URL: "https://script.google.com/macros/s/AKfycbxP2bkPzJkSvYY0uN9DHHpmYCGDlRH0NUjZ381TXjTPlbPfbEnkKigxXBt2rObhJjxO/exec",  // ← Sirf yahan URL daalo
+  COMPANY_NAME: "Litpax Technology Pvt. Ltd.",
+  SYSTEM_NAME: "Employee Advance System",
+};
